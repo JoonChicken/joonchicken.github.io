606e3ceafb5049b55efef224f3e9dea634ea6083
JoonChicken <jbsquirrel06@gmail.com>
2025-06-04 01:05:25 -0700

    Draggable yet clickable headers and x button

--- scripts/draggable_windows.js
+++ scripts/draggable_windows.js
@@ -1,5 +1,15 @@
 // ==============  Body Stuff  =========================
 
 $(".content-panel-header").disableSelection();
-$( ".content-panel" ).draggable({handle: ".content-panel-header"});
+$(".panel-header-x").disableSelection();
+$(".content-panel").draggable({handle: ".content-panel-header", cancel: ".panel-header-x"});
 
+const content_panels = $("content-panel");
+
+for (var i = 0; i < content_panels.children().length - 1; i++) {
+    content_panels.children()[i].find(".panel-header-x").click(() => {
+        console.log("panel " + i + " clicked!");
+        var this_panel = content_panels.children()[i];
+        this_panel.hide();
+    });
+}
