cfc5d922de14cb28afb87a21f6fb088d6c8328d2
JoonChicken <jbsquirrel06@gmail.com>
2025-06-05 01:05:37 -0700

    Content window beautification
    - Made custom icons for headers
    - Windows initialize to some percent of parent div at start and stay that way for consistent content sizing

--- scripts/draggable_windows.js
+++ scripts/draggable_windows.js
@@ -2,14 +2,13 @@
 
 $(".content-panel-header").disableSelection();
 $(".panel-header-x").disableSelection();
+
+const content_panels = document.getElementsByClassName("content-panel");
+
+for (var i = 0; i < content_panels.length; i++) {
+    content_panels[i].style.width = "" + content_panels[i].clientWidth + "px";
+    content_panels[i].style.height = "" + content_panels[i].clientHeight + "px";
+}
+
 $(".content-panel").draggable({handle: ".content-panel-header", cancel: ".panel-header-x"});
 
-const content_panels = $("content-panel");
-
-for (var i = 0; i < content_panels.children().length - 1; i++) {
-    content_panels.children()[i].find(".panel-header-x").click(() => {
-        console.log("panel " + i + " clicked!");
-        var this_panel = content_panels.children()[i];
-        this_panel.hide();
-    });
-}
