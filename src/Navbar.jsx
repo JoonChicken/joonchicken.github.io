3296c684c1e4198eaf0ab81b510d37d2a035f3b9
JoonChicken <jbsquirrel06@gmail.com>
2025-07-01 00:47:21 -0700

    Navbar improvements
    - Added back/forward enabled/disabled icons
    - Disabled buttons look different and cannot be clicked

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,24 +2,24 @@ export default function Navbar() {
     return (
         <section className="explorer-controls-row explorer-navbar">
             <div className="tactile-bump">&nbsp;</div>
-            <button className="explorer-nav-button back-button" aria-label="back button">
-                <div className="explorer-nav-button-inner">
-                    <img/>
+            <div className="explorer-nav-button-outer">
+                <button className="explorer-nav-button back-button" aria-label="back button">
+                    <img className="explorer-nav-button-icon" src="/images/back_button.png"/>
                     <p>Back</p>
-                </div>
-            </button>
-            <button className="explorer-nav-button forward-button" aria-label="forward button">
-                <div className="explorer-nav-button-inner">
-                    <img/>
+                </button>
+            </div>
+            <div className="explorer-nav-button-outer" disabled>
+                <button className="explorer-nav-button forward-button" disabled aria-label="forward button">
+                    <img className="explorer-nav-button-icon" src="/images/forward_button_disabled.png"/>
                     <p>Forward</p>
-                </div>
-            </button>
-            <button className="explorer-nav-button up-button" aria-label="move up directory button">
-                <div className="explorer-nav-button-inner">
-                    <img/>
+                </button>
+            </div>
+            <div className="explorer-nav-button-outer">
+                <button className="explorer-nav-button up-button" aria-label="move up directory button">
+                    <img className="explorer-nav-button-icon" src="/images/back_button.png"/>
                     <p>Up</p>
-                </div>
-            </button>
+                </button>
+            </div>
         </section>
     );
 }
