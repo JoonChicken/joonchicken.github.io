5a069f1014baaada6ca5236d631052b0f9aab740
JoonChicken <jbsquirrel06@gmail.com>
2025-07-09 22:08:02 -0700

    Reinstalled react :(

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,16 +1,18 @@
 export default function Item({additionalClassName, src, name, onClick, onItemDoubleClick, onTab}) {
     return (
-        <button className={"explorer-item " + additionalClassName} onClick={onClick} onDoubleClick={onItemDoubleClick}
-                onKeyUp={(e) => {
-                    if (e.key === "Enter") {
-                        onItemDoubleClick(e);
-                    } else if (e.key == "Tab") {
-                        onTab();
-                    }
-                }}
-        >
-            <img src={src} />
-            <p>{name}</p>
-        </button>
+        <div className="explorer-item-spacer">
+            <button className={"explorer-item " + additionalClassName} onClick={onClick} onDoubleClick={onItemDoubleClick}
+                    onKeyUp={(e) => {
+                        if (e.key === "Enter") {
+                            onItemDoubleClick(e);
+                        } else if (e.key == "Tab") {
+                            onTab();
+                        }
+                    }}
+            >
+                <img src={src} />
+                <p>{name}</p>
+            </button>
+        </div>
     );
 }
