795ee4cd4ef04790e8fac89418711185e64f7567
JoonChicken <jbsquirrel06@gmail.com>
2025-07-10 01:19:42 -0700

    Touch support that actually works

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,20 +2,20 @@ export default function Navbar({onGoBack, onGoForward, onGoUp, backDisabled, for
     return (
         <section className="explorer-controls-row explorer-navbar">
             <div className="tactile-bump">&nbsp;</div>
-            <button className="explorer-nav-button back-button" onClick={onGoBack} onTouchEnd={onGoBack} disabled={backDisabled} aria-label="back button">
+            <button className="explorer-nav-button back-button" onClick={onGoBack} disabled={backDisabled} aria-label="back button">
                 <div className="explorer-nav-button-inner">
                     <img className="explorer-nav-button-icon" src={`/images/back_button${backDisabled ? "_disabled" : ""}.png`}/>
                     <p>Back</p>
                 </div>
             </button>
             
-            <button className="explorer-nav-button forward-button" onClick={onGoForward} onTouchEnd={onGoForward} disabled={forwardDisabled} aria-label="forward button">
+            <button className="explorer-nav-button forward-button" onClick={onGoForward} disabled={forwardDisabled} aria-label="forward button">
                 <div className="explorer-nav-button-inner">
                     <img className="explorer-nav-button-icon" src={`/images/forward_button${forwardDisabled ? "_disabled" : ""}.png`}/>
                     <p>Forward</p>
                 </div>
             </button>
-            <button className="explorer-nav-button up-button" onClick={onGoUp} onTouchEnd={onGoUp} disabled={upDisabled} aria-label="move up directory button">
+            <button className="explorer-nav-button up-button" onClick={onGoUp} disabled={upDisabled} aria-label="move up directory button">
                 <div className="explorer-nav-button-inner">
                     <img className="explorer-nav-button-icon" src={`/images/up_button${upDisabled ? "_disabled" : ""}.png`}/>
                     <p>Up</p>
