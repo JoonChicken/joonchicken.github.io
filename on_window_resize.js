98cf12c0ab5d574b48fb9a2c6027eccdfaae410e
JoonChicken <jbsquirrel06@gmail.com>
2026-01-20 00:30:09 -0500

    more folders

--- on_window_resize.js
+++ on_window_resize.js
@@ -24,7 +24,7 @@ onresize = () => {
     drawBackground();
 }
 
-
+// lerp my beloved
 const lerp = (x, y, a) => x * (1 - a) + y * a;
 const clamp = (a, min = 0, max = 1) => Math.min(max, Math.max(min, a));
 const invlerp = (x, y, a) => clamp((a - x) / (y - x));   
